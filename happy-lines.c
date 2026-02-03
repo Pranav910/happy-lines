e0e4e0ba444ed5bb9df1eacb78a8c50be14a3b70
Pranav Suryavanshi <pranavs910@Pranavs-MacBook-Air.local>
2026-02-03 22:38:20 +0530

    feat: ignore/exclude folders

--- happy-lines.c
+++ happy-lines.c
@@ -27,15 +27,18 @@ int is_directory(const char *path) {
 
 int is_file(const char *path) {
   struct stat st;
+
   if (stat(path, &st) != 0) {
     return 0;
   }
+
   return S_ISREG(st.st_mode);
 }
 
 int count_lines(const char *path) {
   FILE *fp = fopen(path, "r");
   int happy_lines_count = 0;
+
   if (fp == NULL) {
     printf("Error opening file %s\n", "example_dir/example.txt");
     return 1;
@@ -45,16 +48,28 @@ int count_lines(const char *path) {
     if (c == '\n')
       happy_lines_count++;
   }
+
   ++happy_lines_count;
   fclose(fp);
+
   return happy_lines_count;
 }
 
-void count_happy_lines(const char *path, int *total_happy_lines_count,
-                       DIR *dr) {
+void count_happy_lines(const char *path, const char *cwd,
+                       int *total_happy_lines_count, DIR *dr,
+                       char ignore_folders[100][100],
+                       int ignore_folders_count) {
+
+  for (int i = 0; i < ignore_folders_count; i++) {
+    if (strcmp(cwd, ignore_folders[i]) == 0) {
+      return;
+    }
+  }
+
   if (!dr) {
     return;
   }
+
   struct dirent *de;
   char full_path[1000];
 
@@ -64,8 +79,9 @@ void count_happy_lines(const char *path, int *total_happy_lines_count,
     }
     snprintf(full_path, sizeof(full_path), "%s/%s", path, de->d_name);
     if (is_directory(full_path)) {
-      count_happy_lines(full_path, total_happy_lines_count, opendir(full_path));
-      strcpy(full_path, path);
+      count_happy_lines(full_path, de->d_name, total_happy_lines_count,
+                        opendir(full_path), ignore_folders,
+                        ignore_folders_count);
     } else if (is_file(full_path)) {
       *total_happy_lines_count += count_lines(full_path);
     }
@@ -118,6 +134,7 @@ void draw_at(int y, int x, const char *fmt, ...) {
   char buf[1000];
   int offset = sprintf(buf, DRAW_AT_FORMAT, y, x);
   va_list args;
+  
   va_start(args, fmt);
   offset += vsnprintf(buf + offset, sizeof(buf) - offset, fmt, args);
   va_end(args);
@@ -163,6 +180,21 @@ void restore_terminal() {
 }
 
 int draw_menu() {
+  char ignore_folders[100][100];
+  int ignore_folders_count = 0;
+  char ignore_folder[100];
+
+  printf("Enter the folders to ignore (exit to stop): \n");
+
+  while (1) {
+    scanf("%s", ignore_folder);
+    if (strcmp(ignore_folder, "exit") == 0) {
+      break;
+    }
+    strcpy(ignore_folders[ignore_folders_count], ignore_folder);
+    ignore_folders_count++;
+  }
+
   struct dirent *de;
   char path[100] = ".";
   DIR *dr = opendir(path);
@@ -179,7 +211,9 @@ int draw_menu() {
 
   char **arr = (char **)malloc(sizeof(char *) * total_directories);
   int i = 0;
+
   rewinddir(dr);
+
   while ((de = readdir(dr)) != NULL) {
     if (is_directory(de->d_name) && strcmp(de->d_name, "..") != 0) {
       arr[i] = (char *)malloc(sizeof(char) * strlen(de->d_name));
@@ -193,9 +227,11 @@ int draw_menu() {
   while (1) {
     clear_screen();
     draw_at(MAIN_HEADER_TITLE_Y, MAIN_HEADER_TITLE_X, "Select a directory:");
+
     for (int j = 0; j < total_directories; j++) {
       draw_at(j + 2, x, arr[j]);
     }
+
     draw_at(y, 1, ">");
     write(STDOUT_FILENO, "\x1b[H", 3);
 
@@ -210,15 +246,15 @@ int draw_menu() {
     if (key == KEY_UP && y > 2)
       y--;
   }
+
   clear_screen();
 
   draw_at(1, 0, "Selected directory: %s", arr[y - 2]);
-  if (strcmp(path, ".") == 0) {
-    strcat(strcat(path, "/"), arr[y - 2]);
-  }
 
-  dr = opendir(path);
-  count_happy_lines(path, &total_happy_lines_count, dr);
+  dr = opendir(arr[y - 2]);
+
+  count_happy_lines(arr[y - 2], arr[y - 2], &total_happy_lines_count, dr,
+                    ignore_folders, ignore_folders_count);
 
   draw_at(2, 0, "Total happy lines count: %d", total_happy_lines_count);
 
